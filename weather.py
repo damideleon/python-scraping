9e83da15e285ca570c6f09297af816263de895dd
dami_deleon <damideleon97@gmail.com>
2021-12-02 08:08:27 -0300

    Delete apikey

--- weather.py
+++ weather.py
@@ -1,5 +1,5 @@
 ##api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}&lang=es
 import requests
-r = requests.get("http://api.openweathermap.org/data/2.5/weather?q=asuncion&appid=36234f369816fa7c206cf3af2b2c38bc&lang=es").json()
+r = requests.get("http://api.openweathermap.org/data/2.5/weather?q=asuncion&appid=[apiKEY]&lang=es").json()
 t = r['main']['temp'] 
 print(round(t-273.15))
